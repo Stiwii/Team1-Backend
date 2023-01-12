67d13dd96ce0e44fe9af0142e772fa9443c76646
Gilbert3052 <gilbertfernandez3052@gmail.com>
2023-01-12 16:33:48 -0400

    routes created

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        <div className="Logo">
+            <h1>Para Cuando?</h1>
+        </div>
+       <Link href='/'>Home </Link> 
+       <Link href='/login/Login'>Login </Link> 
+       <Link href='/SignUp/SignUp'>Sign Up </Link> 
+       <Link href='/Details/Details'>Details </Link> 
+       <Link href='/Marcas/Marcas'>Marcas </Link> 
+       <Link href='/Perfil/Perfil'>Perfil </Link> 
+       <Link href='/Create/Create'>Create </Link> 
+    </nav>
   )
 }
 
