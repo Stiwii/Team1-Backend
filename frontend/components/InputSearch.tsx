1660dabd9443c0686cc4ccb0182abb7d2a648d7a
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-12 22:06:43 -0300

    search bar added

--- frontend/components/InputSearch.tsx
+++ frontend/components/InputSearch.tsx
@@ -7,6 +7,9 @@ const InputSearch = () => {
         <form className='search flex justify-center mb-20 w-96'>
             <div className='flex relative p-1 rounded-full w-full'>
                 <input id='search' type="text" placeholder='¿Que quieres ver en tu ciudad?' className='search-input flex-1 p-2 rounded-full bg-cyan-50 bi'/>
+                <button type='submit' className='search-button'>
+
+                </button>
             </div>
         </form>
     )
