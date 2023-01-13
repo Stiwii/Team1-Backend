3e2bdad10df45994549f3e74f2ff44174af18332
ItaloKornejo <italokornejo@gmail.com>
2023-01-12 22:24:23 -0500

    feat: start configuration

---
+++ backend/database/models/users.js
@@ -0,0 +1,68 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+		/*
+			Siempre empezaremos definiendo el modelo Js a referenciar
+			Seguido de los datos: 
+			as: que servirá de alias en las consultas y mixins
+			
+			foreignKey: Aquí especificamos de acuerdo con la función.
+				1.- belongsTo: 'El campo en esta tabla que es FK es:'
+				2.- hasOne, hasMany, belongsToMany: 'El campo en la otra tabla que es FK es:'
+
+			through: Solo funciona en belongsToMany, especiifica la tabla pivote usando el modelo js
+		*/
+      // Users.belongsTo(models.MODELNAME1, {as: 'country', foreignKey: 'country_id'})
+      // Users.hasOne(models.MODELNAME2, {as: 'team', foreignKey: 'user_id'})
+      // Users.hasMany(models.MODELNAME3, {as: 'certificates', foreignKey: 'user_id'})
+      // Users.belongsToMany(models.MODELNAME4, {as: 'votes', through: models.Votes, foreignKey: 'user_id'})
+			// Consejo avanzado, esta aquí por si más adelante hay una lección.
+			// Algunas veces, el scope tendrá includes
+      // para evitar errores es usual usarlo así
+      // Users.addScope('scope_name', {});
+    }
+  };
+  Users.init({
+    id: {
+      type: DataTypes.UUID, 
+      primaryKey: true
+    },
+		country_id: {
+		type: DataTypes.UUID,
+		},
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'Users',  // Hacemos la diferencia del modelo
+    tableName: 'users',  // y la tabla en la DB para ser explicitos
+    underscored: true,  
+    timestamps: true,
+    // Los scopes son útiles para estandarizar dónde se regresa información  
+		// y minimizar que se nos escape algo
+		scopes: {
+      public_view: {
+        attributes: ['id','country_id']
+      },
+      no_timestamps: {
+        attributes: {exclude: ['created_at', 'updated_at']}
+      },
+    },
+  });
+  return Users;
+};
---
+++ backend/routes/models.router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routesUsers = require('./users.routes')
+
+function routerModels(app) {
+    const router = express.Router();
+    
+    app.use('/api/v1', router);
+
+    router.use('/users',routesUsers)
+		// other models here
+  }
+  
+  module.exports = routerModels;
