2fff7ccf2aee458d2a1d4cd88aaf326966210071
Gilbert3052 <gilbertfernandez3052@gmail.com>
2023-01-16 17:30:21 -0400

    eventCard created

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { ReactElement } from 'react'
+import EventCard from '../components/EventCard'
 import Layout from '../components/Layout'
 import NestedLayout from '../components/NestedLayout'
 import type { NextPageWithLayout } from './_app'
 
 const Page: NextPageWithLayout = () => {
-  return <p>hello world</p>
+  return <EventCard></EventCard>
 }
 
 Page.getLayout = function getLayout(page: ReactElement) {
