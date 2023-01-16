ae360b0404e40cafb8e088e102bc2880633bb1f5
Gilbert3052 <gilbertfernandez3052@gmail.com>
2023-01-16 11:12:47 -0400

    routes

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -7,15 +7,15 @@ const Navbar = ({}) => {
         <div className="Logo">
             <h1>Para Cuando?</h1>
         </div>
-       <Link href='/'>Home </Link> 
-       <Link href='/login/Login'>Login </Link> 
-       <Link href='/SignUp/SignUp'>Sign Up </Link> 
-       <Link href='/Details/[event_id]'>Details </Link> 
-       <Link href='/Brands'>Brands </Link> 
-       <Link href='/Artists'>Artists </Link> 
-       <Link href='/Tournaments'>Tournaments </Link> 
-       <Link href='/Profile'>Profile </Link> 
-       <Link href='/Create'>Create </Link> 
+        <Link href='/'>Home </Link> 
+        <Link href='/login'>Login </Link> 
+        <Link href='/SignUp'>Sign Up </Link> 
+        <Link href='/Details/[event_id]'>Details </Link> 
+        <Link href='/Brands'>Brands </Link> 
+        <Link href='/Artists'>Artists </Link> 
+        <Link href='/Tournaments'>Tournaments </Link> 
+        <Link href='/Profile'>Profile </Link> 
+        <Link href='/Create'>Create </Link> 
     </nav>
   )
 }
