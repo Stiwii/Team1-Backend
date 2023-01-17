4f56c87d741771c479ba51528473e57013f8891d
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-17 16:42:51 -0300

    "footer" created

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,13 +1,23 @@
 import React, {useState, useEffect} from 'react';
-import Image from 'next/image';
-import cosas from '/images/rectangle.png'
+import Image from "next/image"
+import { kMaxLength } from 'buffer';
+import InputSearch from './InputSearch';
 
 const Footer = () => {
 
   return (
-    <footer>
-      <img src="/images/rectangle21.png" alt='footer-image'/>
-      <img src={`cosas`} alt='footer-image'/>
+    <footer className='bottom-0 left-0 flex flex-col items-center justify-center relative'>
+      <div className=''>
+        <Image 
+          src="/images/footerImage.png" 
+          alt='footer-image' 
+          width={kMaxLength} 
+          height={kMaxLength}
+        />
+      </div>
+      <div className='w-[100%] flex justify-center items-center absolute p-[26px]'>
+        <InputSearch/>
+      </div>
     </footer>
   )
 }
