895951a08245152cda9b5575a597de6fd14763e3
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-17 22:48:57 -0300

    changes

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-const Navbar = ({}) => {
+const Navbar = () => {
   return (
     <nav>
         <div className='h-[200px] justify-center items-center'>
